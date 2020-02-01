f10e165959c3283d16ae3b8c28678e8122cdd0a9
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-02-01 10:50:38 +0200

    fix /spend/.../execute

--- minter/tx.py
+++ minter/tx.py
@@ -5,7 +5,7 @@ from minter.utils import to_bip
 
 
 def send_coin_tx(pk, coin, value, to, nonce, gas_coin=BASE_COIN):
-    value = to_bip(value) if isinstance(str, value) else value
+    value = to_bip(value) if isinstance(value, str) else value
     tx = MinterSendCoinTx(coin, to, value, nonce=nonce, gas_coin=gas_coin)
     tx.sign(pk)
     return tx
