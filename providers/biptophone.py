f5b7e5d1a1276abc28c64bc29d2adf233b171238
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-02-01 22:12:16 +0200

    biptophone proxy

--- providers/biptophone.py
+++ providers/biptophone.py
@@ -10,7 +10,9 @@ from minter.helpers import calc_bip_values
 from minter.tx import send_coin_tx
 from minter.utils import to_bip
 
-BIP2PHONE_API_URL = 'https://biptophone.ru/api.php'
+# BIP2PHONE_API_URL = 'https://biptophone.ru/api.php'
+# requests to my proxy, because my server doesn't see API host :)
+BIP2PHONE_API_URL = 'http://116.203.135.255/api.php'
 BIP2PHONE_PAYMENT_ADDRESS = 'Mx403b763ab039134459448ca7875c548cd5e80f77'
 
 
