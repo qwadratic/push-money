4ae6e9e5be04fe650810bc4b69e159f794e2b159
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-02-02 14:58:58 +0200

    dev CORS headers

--- api/app_init.py
+++ api/app_init.py
@@ -6,6 +6,12 @@ from api.root import bp_root
 
 def app_init():
     app = Flask(__name__)
+
+    @app.after_request
+    def handle_global_response(response):
+        response.headers.extend({
+            'Access-Control-Allow-Origin': '*',
+        })
     app.register_blueprint(bp_api)
     app.register_blueprint(bp_root)
     return app
