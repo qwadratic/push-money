725801e9c52b1c6f2748f29baee30b5691b9fce7
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-02-08 00:45:28 +0200

    webhook view response

--- api/webhooks.py
+++ api/webhooks.py
@@ -8,3 +8,4 @@ bp_webhooks = Blueprint('webhook', __name__, url_prefix='/webhooks')
 @bp_webhooks.route('/gift/<order_id>', methods=['POST'])
 def gift_order_result(order_id):
     gift_webhook_controller(request, order_id)
+    return ''
