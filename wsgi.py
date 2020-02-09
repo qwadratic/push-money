76f4762830072fb4d3a00553124888a699f22aeb
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-02-09 08:32:44 +0200

    run scheduler

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 from api.app_init import app_init
 from jobs.scheduler import scheduler
 
-
+scheduler.start()
 app = app_init()
 
 
 if __name__ == '__main__':
-    scheduler.start()
+
     app.run(debug=True, port=8000)
