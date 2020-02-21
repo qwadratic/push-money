d1b9e3fa9260ecc23c8cfb3839c6318d3e6442c8
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-02-21 14:07:03 +0200

    fix migrator

--- migrate.py
+++ migrate.py
@@ -1,5 +1,7 @@
-from api.models import database
+from api.models import db
 
 
 if __name__ == '__main__':
-    database.evolve(ignore_tables=['basemodel', 'passwordprotectedmodel'])
+    from wsgi import app
+    with app.app_context():
+        db.database.evolve(ignore_tables=['basemodel', 'passwordprotectedmodel'])
