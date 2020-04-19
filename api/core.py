79b91ecac378dd412ff754e78d55057bd26e6772
Ivan Kotelnikov <ivan.kotelnikov@bitfury.com>
2020-04-19 04:22:22 +0300

    add rouble to coin list

--- api/core.py
+++ api/core.py
@@ -22,7 +22,7 @@ def health():
 
 @bp_api.route('/custom-coins')
 def custom_coins():
-    return jsonify({"symbols": ["DICE", "TIME", "UNUCOIN", "PIZZA", "POPE"]})
+    return jsonify({"symbols": ["ROUBLE", "DICE", "TIME", "UNUCOIN", "PIZZA", "POPE"]})
 
 
 @bp_api.route('/exchange-rates')
